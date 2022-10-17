2c7a7d20f9d322ca5dee7c47f324ad3f95e1d4f8
yanun <yanuni@qq.com>
2022-10-17 17:21:35 +0800

    Initial Commit

---
+++ iOSCodeRepo/iOSCodeRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCodeRepoApp.swift
+//  iOSCodeRepo
+//
+//  Created by 丁燕军 on 2022/10/17.
+//
+
+import SwiftUI
+
+@main
+struct iOSCodeRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
