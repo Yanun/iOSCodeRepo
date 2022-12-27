c3e56dc3fa44b5f072bb237a7911a83054b43dab
yanun <yanuni@qq.com>
2022-12-27 16:30:26 +0800

    add view controller extension

---
+++ iOSCodeRepo/Extension/UIViewController+.swift
@@ -0,0 +1,26 @@
+//
+//  UIViewController+.swift
+//  iOSCodeRepo
+//
+//  Created by 丁燕军 on 2022/12/27.
+//
+
+import UIKit
+
+extension UIViewController {
+    func add(_ child: UIViewController) {
+        addChild(child)
+        view.addSubview(child.view)
+        child.didMove(toParent: self)
+    }
+
+    func remove() {
+        guard parent != nil else {
+            return
+        }
+
+        willMove(toParent: nil)
+        view.removeFromSuperview()
+        removeFromParent()
+    }
+}
