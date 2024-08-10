463cf7ecca797a3fea9c0c62bf3ff1952637b398
yanun <yanuni@qq.com>
2024-08-10 18:17:34 +0800

    add .h file

---
+++ iOSCodeRepo/Extension/misc+.h
@@ -0,0 +1,18 @@
+//
+//  misc+.h
+//  iOSCodeRepo
+//
+//  Created by 丁燕军 on 2024/8/10.
+//
+
+#ifndef misc__h
+#define misc__h
+
+@interface CALayer ()
+
+// view.layer.setDisableUpdateMask(0x12)
+- (void)setDisableUpdateMask:(unsigned int)aValue;
+
+@end
+
+#endif /* misc__h */
